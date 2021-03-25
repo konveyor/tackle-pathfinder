f0c935c4ac8df644fa7f9e2d74d7ae395c72d508
Jonathan Vila <jvilalop@redhat.com>
2021-03-25 17:24:11 +0100

    debug
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

--- .github/scripts/check_deployment.sh
+++ .github/scripts/check_deployment.sh
@@ -1,6 +1,18 @@
 #!/bin/sh -x
 
 while [[ ! "$(kubectl rollout status deployment tackle-pathfinder -n tackle)" =~ "successfully" ]]; do
-  kubectl rollout status deployment tackle-pathfinder -n tackle
+  echo "$(kubectl rollout status deployment tackle-pathfinder -n tackle)"
   sleep 15
 done
+
+kubectl rollout status deployment tackle-pathfinder -n tackle
+kubectl describe deployment tackle-pathfinder -n tackle
+
+kubectl get pods -n tackle
+kubectl get pods -n microcks
+echo "---------------------------------------------------------------"
+echo "---------------------------------------------------------------"
+kubectl logs --tail=100 -l app.kubernetes.io/name=tackle-pathfinder -n tackle
+echo "---------------------------------------------------------------"
+echo "---------------------------------------------------------------"
+kubectl logs --tail=100 -l app.kubernetes.io/name=keycloak -n tackle
