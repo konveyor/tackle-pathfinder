f44d0a07f1ac6b0d8f60396fbcbf2d9cf113f2ca
Jonathan Vila <jvilalop@redhat.com>
2021-03-25 16:48:11 +0100

    split command to check deployment
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

---
+++ .github/scripts/check_deployment.sh
@@ -0,0 +1,4 @@
+while [[ ! "$(kubectl rollout status deployment tackle-pathfinder -n tackle)" =~ "successfully" ]]; do
+  kubectl rollout status deployment tackle-pathfinder -n tackle
+  sleep 15
+done
