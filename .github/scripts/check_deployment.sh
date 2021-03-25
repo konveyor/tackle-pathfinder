18b77f5fc4e0c5fb028699abe94b95503a6d6c2b
Jonathan Vila <jvilalop@redhat.com>
2021-03-25 17:03:28 +0100

    debug
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

--- .github/scripts/check_deployment.sh
+++ .github/scripts/check_deployment.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -x
+
 while [[ ! "$(kubectl rollout status deployment tackle-pathfinder -n tackle)" =~ "successfully" ]]; do
   kubectl rollout status deployment tackle-pathfinder -n tackle
   sleep 15
