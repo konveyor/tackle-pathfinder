a87d6d4b402c4da9c451454b1abaa1f5445b0204
Jonathan Vila <jvilalop@redhat.com>
2021-04-07 19:24:42 +0200

    Added AbstractEntity
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

---
+++ src/main/java/io/tackle/pathfinder/model/AbstractEntity.java
@@ -0,0 +1,64 @@
+package io.tackle.pathfinder.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.security.identity.SecurityIdentity;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.enterprise.inject.spi.CDI;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.Instant;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity extends PanacheEntity {
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(updatable=false)
+    public Instant createTime;
+    
+    public String createUser;
+    
+    @JsonIgnore
+    @UpdateTimestamp
+    public Instant updateTime;
+    
+    public String updateUser;
+    
+    @JsonIgnore
+    @Column(name = "deleted", columnDefinition = "BOOLEAN NOT NULL DEFAULT FALSE")
+    public Boolean deleted = false;
+
+    @PrePersist
+    protected void onCreate() {
+        final String username = getUsername();
+        createUser = username;
+        updateUser = username;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updateUser = getUsername();
+    }
+
+    private String getUsername() {
+        // based on the Quarkus issue
+        // https://github.com/quarkusio/quarkus/issues/6948#issuecomment-619872942
+        SecurityIdentity context = CDI.current().select(SecurityIdentity.class).get();
+        if (Objects.nonNull(context)) {
+            String username = context.getPrincipal().getName();
+            if (Objects.nonNull(username)) {
+                return username;
+            }
+        }
+        // since all service are authenticated, it should never get here
+        // so maybe worth evaluating to throw an exception? food for thoughts
+        return "";
+    }
+
+}
+
