b6e0e6e2b36f5eb436db02c0b5393c99198db638
Jonathan Vila <jvilalop@redhat.com>
2021-05-11 12:21:10 +0200

    Added ValidationException to control the SQL constraint errors

---
+++ src/main/java/io/tackle/pathfinder/exception/ValidationExceptionMapper.java
@@ -0,0 +1,14 @@
+package io.tackle.pathfinder.exception;
+
+import javax.validation.ValidationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {
+    @Override
+    public Response toResponse(ValidationException exception) {
+        return Response.status(400).build();
+    }
+}
