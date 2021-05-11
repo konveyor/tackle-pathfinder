106a975f92d620db5d5462703b55450a4e977cb9
Jonathan Vila <jvilalop@redhat.com>
2021-05-11 12:57:57 +0200

    Adding ConstaintViolationException mapper

---
+++ src/main/java/io/tackle/pathfinder/exception/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,14 @@
+package io.tackle.pathfinder.exception;
+
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+        return Response.status(400).build();
+    }
+}
