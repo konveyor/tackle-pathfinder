3200ff88ec4d9125897a1af39f1bf248e248e3dd
mrizzi <mrizzi@redhat.com>
2022-04-28 17:56:53 +0200

    TACKLE-507 Update Tackle Repo licenses to APL 2.0

--- src/main/resources/db/migration/V0.0.3_001__revise_wording_questionnaire.sql
+++ src/main/resources/db/migration/V0.0.3_001__revise_wording_questionnaire.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright © 2021 Konveyor (https://konveyor.io/)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 update category set name='Application details' where name='Application Details';
 update question set description ='How much knowledge does the team have about the application''s development or usage?' where description='How much knowledge does the team have regarding the development or usage of the application';
 update single_option set option='Little knowledge, no development (example: third-party or commercial off-the-shelf application)' where option='External 3rd party or COTS application ';
