b25a8b761ab1a5ecf968d00b902e9289249f0ac7
mrizzi <mrizzi@redhat.com>
2022-04-29 10:17:16 +0200

    TACKLE-507 Update Tackle Repo licenses to APL 2.0

--- src/main/resources/db/migration/V0.0.4_001__create_i18n.sql
+++ src/main/resources/db/migration/V0.0.4_001__create_i18n.sql
@@ -1,5 +1,5 @@
 --
--- Copyright © 2021 Konveyor (https://konveyor.io/)
+-- Copyright © 2021 the Konveyor Contributors (https://konveyor.io/)
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
